3188c9bec68c59d981e14905f4efae93d1e63716
Juanan Antona <juan.antonadelval@gmail.com>
2019-11-16 18:15:04 +0100

    Added bodyparser and logger

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const Koa = require("koa");
-const Router = require("koa-router");
+const Koa = require('koa');
+const Router = require('koa-router');
+const BodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
 
-router.get("/", async function (ctx) {
-    ctx.body = {message: "Hello World!"}
+// Use the bodyparser middlware
+app.use(BodyParser());
+
+const logger = require('koa-logger');
+app.use(logger());
+
+router.get('/', async function(ctx) {
+  ctx.body = { message: 'Hello World!' };
 });
 
 app.use(router.routes()).use(router.allowedMethods());

d82fbe572b05f8f070a7716a4a644efdfad57e1b
Juanan Antona <juan.antonadelval@gmail.com>
2019-11-17 10:54:17 +0100

    Basic CRUD

--- index.js
+++ index.js
@@ -15,6 +15,35 @@ router.get('/', async function(ctx) {
   ctx.body = { message: 'Hello World!' };
 });
 
+// List all brothers
+router.get('/brothers', async ctx => {
+  ctx.body = await ctx.app.brothers.find().toArray();
+});
+
+// Create new brother
+router.post('/brothers', async ctx => {
+  ctx.body = await ctx.app.brothers.insert(ctx.request.body);
+});
+
+const ObjectID = require('mongodb').ObjectID;
+// Get one
+router.get('/brothers/:id', async ctx => {
+  ctx.body = await ctx.app.brothers.findOne({ _id: ObjectID(ctx.params.id) });
+});
+
+// Update one
+router.put('/brothers/:id', async ctx => {
+  let documentQuery = { _id: ObjectID(ctx.params.id) };
+  let valuesToUpdate = { $set: ctx.request.body };
+  ctx.body = await ctx.app.brothers.updateOne(documentQuery, valuesToUpdate);
+});
+
+// Delete one
+router.delete('/brothers/:id', async ctx => {
+  let documentQuery = { _id: ObjectID(ctx.params.id) };
+  ctx.body = await ctx.app.brothers.deleteOne(documentQuery);
+});
+
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
